f744504573010724111bc27ddd381ee5b62da744
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-12 23:45:20 +0100

    finally working! next step split this into class or function didin't decided yet

--- SearchXML/SearchXML.cpp
+++ SearchXML/SearchXML.cpp
@@ -9,16 +9,52 @@ using namespace std;
 
 int main()
 {
-	std::string stXML = "123456789Person123456789Person123456789Person123456789Person"; // "<Table1><Person>Hello</Person><Person>JoeSpaggetti</Person></Table1>";
+	std::string stXML = "<Table1><Person>Ed</Person><Person>Joe</Person></Table1>";
+	//F 8, 30 | 16, 38
+	//B 21 41 | 30, 50 
+
+	int tempLenght = 0; 
+	tempLenght = stXML.length()/2;
+	unsigned int foundLocationFront[20]{ 0 };
+	unsigned int foundLocationBack[20]{ 0 };
+	int differenceLoc[5]{ 0 };
+	int pharse = 8; //<Person>
 
 
-	int tempLenght = 0;
 
-	tempLenght = stXML.length()/4;
-
-	for (int i = 0; i < stXML.length(); i += tempLenght)
+	//feel array with position number where words starts
+	for ( int i = 0, j =  0, k = 0; i < tempLenght; k++, j++, i++)
 	{
-		std::cout << "i:" << i << " " << stXML.find("Person", i) << std::endl;
+		if (i < tempLenght)
+		{
+			foundLocationFront[j] = stXML.find("<Person>", i);
+			foundLocationBack[j] = stXML.find("</Person>", k);
+			
+			i = foundLocationFront[j]; // adjusting i to start search after first found number
+			k = foundLocationBack[j];
+
+			foundLocationFront[j] += pharse;  // adding lenght of word <Person>
+			
+		}
+	}
+
+	// Working out the difference
+	for ( int i = 0; i < 2; i++)
+	{
+		differenceLoc[i] = foundLocationBack[i] - foundLocationFront[i];
+		//differenceLoc[j] -= pharse; // lenght of word Person
+	}
+
+	int sizeOfArray = sizeof(differenceLoc) / sizeof(int);
+
+	//extract words from string
+	for (int i = 0, j = 1; i < sizeOfArray; j++, i++)
+	{
+
+		if (foundLocationFront[i] > 0)
+		{	
+			std::cout << stXML.substr(foundLocationFront[i], differenceLoc[i]) << std::endl;
+		}
 
 	}
 
@@ -26,7 +62,3 @@ int main()
     return 0;
 }
 
-//string substr(int pos = 0, int n = string::npos) const;
-//string s = "thanks for help, Kathy";
-//cout << s.substr(7, 3) << endl; /* print "for" */
-//cout << s.substr(17) << endl; /* print "Kathy" */
